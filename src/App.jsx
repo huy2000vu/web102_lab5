e3b136aec7411a60ad8bd5705d29f805f312cee5
huy2000vu <huy2000vu@gmail.com>
2024-10-15 22:01:24 -0700

    finished lab

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,22 @@ const api_key = import.meta.env.VITE_APP_API_KEY;
 import CoinInfo from "./components/CoinInfo";
 export default function App() {
   const [list, setList] = useState(null);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) =>
+        Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      );
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(Object.keys(list.Data));
+    }
+  };
   useEffect(() => {
     const fetchAllCoinData = async () => {
       const response = await fetch(
@@ -17,10 +33,24 @@ export default function App() {
   console.log(list); //debug
   return (
     <div className="whole-page">
-      <h1>
-        My Crypto List
-        <ul>
-          {list &&
+      <h1>My Crypto List</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(inputString) => searchItems(inputString.target.value)}
+      />
+      <ul>
+        {searchInput.length > 0
+          ? filteredResults.map((coin) =>
+              list.Data[coin].PlatformType === "blockchain" ? (
+                <CoinInfo
+                  image={list.Data[coin].ImageUrl}
+                  name={list.Data[coin].FullName}
+                  symbol={list.Data[coin].Symbol}
+                />
+              ) : null
+            )
+          : list &&
             Object.entries(list.Data).map(([coin]) =>
               list.Data[coin].PlatformType === "blockchain" ? (
                 <CoinInfo
@@ -30,8 +60,7 @@ export default function App() {
                 />
               ) : null
             )}
-        </ul>
-      </h1>
+      </ul>
     </div>
   );
 }
